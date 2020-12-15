b29cff4b2461cc9c89da6c2ab3824cc9ce99835e
bogdan <marc.bogdan96@yahoo.com>
2020-12-15 19:42:35 +0000

    Refactoring

--- lib/logist/formatter/json.rb
+++ lib/logist/formatter/json.rb
@@ -10,20 +10,13 @@ module Logist
       attr_accessor :flat_json
 
       def call(severity, timestamp, _progname, raw_msg)
-        sev = severity.is_a?(String) && severity.match('FATAL') ? 'ERROR' : severity
-        tstamp = format_datetime(timestamp).strip
-        msg = normalize_message(raw_msg)
+        sev = process_severity(severity)
+        tstamp = process_timestamp(timestamp)
 
         payload = { level: sev, timestamp: tstamp, environment: ::Rails.env }
 
-        if flat_json && msg.is_a?(Hash)
-          payload.merge!(msg)
-        elsif msg.is_a?(String) && msg.match(/Status [0-9]+/)
-          status = msg.split(' ')[1]
-          payload.merge!(message: { status: status })
-        else
-          payload.merge!(message: msg)
-        end
+        msg = process_message(raw_msg)
+        payload.merge!(msg)
 
         payload.to_json << "\n"
       end
@@ -36,6 +29,27 @@ module Logist
         JSON.parse(raw_msg)
       rescue JSON::ParserError
         raw_msg
+      end
+
+      def process_severity(severity)
+        severity.is_a?(String) && severity.match('FATAL') ? 'ERROR' : severity
+      end
+
+      def process_timestamp(timestamp)
+        format_datetime(timestamp).strip
+      end
+
+      def process_message(raw_msg)
+        msg = normalize_message(raw_msg)
+
+        if flat_json && msg.is_a?(Hash)
+          msg
+        elsif msg.is_a?(String) && msg.match(/Status [0-9]+/)
+          status = msg.split(' ')[1]
+          { message: { status: status } }
+        else
+          { message: msg }
+        end
       end
     end
   end
