0eae7edbac681cc0474a66fd6f34c9f75d034c35
bogdan <marc.bogdan96@yahoo.com>
2020-12-15 20:09:19 +0000

    Fixed last commit

--- lib/logist/formatter/json.rb
+++ lib/logist/formatter/json.rb
@@ -52,8 +52,8 @@ module Logist
           method = splitted_msg[0]
           path = splitted_msg[1]
           { message: { method: method, path: path } }
-        elsif msg.is_a?(String) && msg.split(' ')[0].match('User-Agent:') && msg.split('\n')[1].split(' ')[0].match('Accept:')
-          splitted_msg = msg.split('\n')
+        elsif msg.is_a?(String) && msg.split(' ')[0].match('User-Agent:') && msg.split("\n")[1].split(' ')[0].match('Accept:')
+          splitted_msg = msg.split("\n")
           user_agent = splitted_msg[0].split(' ')[1]
           accept = splitted_msg[1].split(' ')[1]
           { message: { user_agent: user_agent, accept: accept } }
